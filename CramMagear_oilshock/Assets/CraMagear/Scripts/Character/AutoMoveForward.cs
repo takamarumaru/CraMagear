8299105ebd53511643eff3e30fc370271db3e188
MorieTakaya <kd1259599@st.kobedenshi.ac.jp>
2022-05-09 13:33:03 +0900

    【Project】 ・プロジェクト全体を再upload

---
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/Character/AutoMoveForward.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoveForward : MonoBehaviour
+{
+    private float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
