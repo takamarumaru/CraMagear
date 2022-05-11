6f339a6ba70744d1a3d66f3d1b6150ffb8ffe4c7
FukazawaTooma <kd1253327@st.kobedenshi.ac.jp>
2022-05-11 14:52:58 +0900

    アニメーション追加

---
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/Animation/StateMachineManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameStateMachine
+{
+    //=====================================
+    /// <summary>
+    /// ステートマシンノード基本クラス
+    /// </summary>
+    //=====================================
+    [System.Serializable]
+    public class StateNodeBase
+    {
+        //このステートに入った時、1度だけ実行
+        public virtual void OnEnter() { }
+
+        //このステートから出る時、１度だけ実行
+        public virtual void OnExit() { }
+
+        //毎フレーム実行
+        public virtual void OnUpdate() { }
+
+        //一定時間ごとに実行
+        public virtual void OnFixedUpdate() { }
+
+        //管理元のマネージャーへの参照
+        StateMachineManager _stateMgr;
+
+        //アニメーターへの参照
+        Animator _animator;
+
+        //ゲッター
+        public StateMachineManager StateMgr => _stateMgr;
+
+        public void Initialize(Animator animator, StateMachineManager mgr)
+        {
+            _animator = animator;
+            _stateMgr = mgr;
+        }
+
+        //仮
+        [SerializeField] int _date;
+    }
+
+    /// <summary>
+    /// ステートマシーン管理クラス
+    /// </summary>
+    public class StateMachineManager : MonoBehaviour
+    {
+        [SerializeField] CharacterBrain _charaBrain;
+
+        public CharacterBrain CharaBrain => _charaBrain;
+
+        //現在のステート
+        StateNodeBase _nowState;
+
+        //ステート変更
+        public void ChangeState(StateNodeBase state)
+        {
+            //?:nullチェックしてくれる自作のみ（MonoBehaverなどでは使用不可）
+            _nowState?.OnExit();
+
+            _nowState = state;
+
+            
+            _nowState?.OnEnter();
+            
+        }
+
+        //更新処理
+        private void Update()
+        {
+            if (_nowState == null) { return; }
+
+            _nowState.OnUpdate();
+
+            _nowState.OnFixedUpdate();
+        }
+    }
+}
--- CramMagear_oilshock/Assets/CraMagear/Scripts/Character/CharacterBrain.cs
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/Character/CharacterBrain.cs
@@ -6,89 +6,195 @@ using UnityEngine;
 [RequireComponent(typeof(OpenCharacterController))]
 public class CharacterBrain : MonoBehaviour
 {
+    //アンダーバー表記がメンバー
+    //Transform _transform;
 
+    //[Tooltip("[--性能のパラメータ--]")]
     [Header("[--性能のパラメータ--]")]
     [SerializeField] float _moveSpeed = 1.0f;
 
-    //キャラクターコントローラー
-    OpenCharacterController _charCtrl;
+    OpenCharacterController _charaCtrl;
 
-    //InputProvider
+    //
     InputProvider _inputProvider;
 
-    BulletShot _bulletShot;
-
-    UnityEngine.InputSystem.PlayerInput _input;
+    [Header("[--Component参照--]")]
+    [SerializeField] Animator _animator;
 
     //速度（ベクトルなど）
     Vector3 _velocity;
 
     private void Awake()
     {
-        _charCtrl = GetComponent<OpenCharacterController>();
-        TryGetComponent(out _input);
+        //_transform = transform;
+        _charaCtrl = GetComponent<OpenCharacterController>();
 
         //自分以下の子のInputProviderを継承したコンポーネントを取得
         _inputProvider = GetComponentInChildren<InputProvider>();
-
-        //銃のコンポーネント取得
-        _bulletShot = GetComponentInChildren<BulletShot>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_inputProvider.GetButtonAttack())
-        {
-            //弾生成、発射
-            _bulletShot.LauncherShot();
-        }
-
-        //入力情報
-        var axisL = _inputProvider.GetAxisL();
-
-        float axisPower = axisL.magnitude;
-        //--------------
-        //向き
-        //--------------
-        if (axisPower > 0.01f)
-        {
-            Vector3 vLook = new Vector3(axisL.x, 0, axisL.y);
-            Quaternion rotation = Quaternion.LookRotation(vLook, Vector3.up);
-
-            transform.rotation = Quaternion.RotateTowards
-                (
-                transform.rotation,   //変化前の回転
-                rotation,                   //変化後の回転
-                720 * Time.deltaTime        //変化する角度
-                );
-        }
-
-        //-----------
-        //移動
-        //-----------
-
-        Vector3 forward = new Vector3(axisL.x, 0, axisL.y);
-        forward.Normalize();
-        forward *= axisPower * _moveSpeed;
-        _velocity = forward * Time.deltaTime;
-        //重力
-        _velocity.y += -9.8f * Time.deltaTime;
-        if (_charCtrl.isGrounded)
-        {
-            _velocity *= 0.85f;
-        }
-        else
-        {
-            _velocity *= 0.99f;
-        }
-
         //移動(秒速)
-        _charCtrl.Move(_velocity * Time.deltaTime);
+        _charaCtrl.Move(_velocity * Time.deltaTime);
 
-        if (_charCtrl.isGrounded)
+        if (_charaCtrl.isGrounded)
         {
             _velocity.y = 0;
+        }
+    }
+
+    /// <summary>
+    /// 立ち状態クラス
+    /// </summary>
+    [System.Serializable]
+    public class ASStand : GameStateMachine.StateNodeBase
+    {
+        public override void OnExit()
+        {
+            base.OnExit();
+            Debug.Log("Exit");
+        }
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            Debug.Log("Stand");
+
+            //var brain = StateMgr.GetComponentInParent<CharacterBrain>();
+
+            var brain = StateMgr.CharaBrain;
+
+            Vector2 axisL = brain._inputProvider.GetAxisL();
+
+            if (axisL.magnitude > 0.1f)
+            {
+                brain._animator.SetBool("IsMoving", true);
+            }
+            //重力
+            brain._velocity.y += -9.8f * Time.deltaTime;
+
+            //攻撃
+            if (brain._inputProvider.GetButtonAttack())
+            {
+                brain._animator.SetTrigger("DoAttack");
+            }
+        }
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+
+            var brain = StateMgr.GetComponentInParent<CharacterBrain>();
+
+            //重力
+            if (brain._charaCtrl.isGrounded)
+            {
+                brain._velocity *= 0.85f;
+            }
+            else
+            {
+                brain._velocity *= 0.98f;
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// 歩き状態クラス
+    /// </summary>
+    [System.Serializable]
+    public class ASWalk : GameStateMachine.StateNodeBase
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Debug.Log("Enter");
+        }
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            Debug.Log("Walk");
+
+            //var brain = StateMgr.GetComponentInParent<CharacterBrain>();
+
+            var brain = StateMgr.CharaBrain;
+
+            Vector2 axisL = brain._inputProvider.GetAxisL();
+            if (axisL.magnitude < 0.1f)
+            {
+                brain._animator.SetBool("IsMoving", false);
+            }
+
+            brain._animator.SetFloat("MoveSpeed", axisL.magnitude);
+
+            float axisPower = axisL.magnitude;
+            //--------------
+            //向き
+            //--------------
+            if (axisPower > 0.01f)
+            {
+                Vector3 vLook = new Vector3(axisL.x, 0, axisL.y);
+                Quaternion rotation = Quaternion.LookRotation(vLook, Vector3.up);
+
+                brain.transform.rotation = Quaternion.RotateTowards
+                    (
+                    brain.transform.rotation,   //変化前の回転
+                    rotation,                   //変化後の回転
+                    720 * Time.deltaTime        //変化する角度
+                    );
+            }
+
+            //--------------
+            //移動
+            //--------------
+
+            Vector3 forward = new Vector3(axisL.x, 0, axisL.y);
+            forward.Normalize();
+
+            forward *= axisPower * brain._moveSpeed;
+
+            brain._velocity += forward * Time.deltaTime;
+
+            //重力
+            brain._velocity.y += -9.8f * Time.deltaTime;
+
+        }
+
+        //固定フレームレートで動く更新
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+
+            var brain = StateMgr.GetComponentInParent<CharacterBrain>();
+
+            //重力
+            if (brain._charaCtrl.isGrounded)
+            {
+                brain._velocity *= 0.85f;
+            }
+            else
+            {
+                brain._velocity *= 0.98f;
+            }
+        }
+    }
+
+    [System.Serializable]
+
+    public class ASAttack : GameStateMachine.StateNodeBase
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+        }
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
         }
     }
 }
