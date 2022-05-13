33bd2e9fb9c6cb9f4bf059820509de5a1e73ec57
yoshizawasouma <kd1254382@st.kobedenshi.ac.jp>
2022-05-13 11:52:57 +0900

    UIHPバー

--- CramMagear_oilshock/Assets/CraMagear/Scripts/UI/PlayerGauge.cs
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/UI/PlayerGauge.cs
@@ -3,64 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+
 public class PlayerGauge : MonoBehaviour
 {
     [SerializeField]
-    private Image GreenGage;
+    private Image GreenGauge;
     [SerializeField]
-    private Image RedGage;
+    private Image RedGauge;
 
-    private SamplePlayerHP player;
+    //private Player player;
     private Tween redGaugeTween;
-
-    float _hp = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     public void GaugeReduction(float reducationValue, float time = 1f)
     {
-        var valueFrom = player.life / player.maxLife;
-        var valueTo = (player.life - reducationValue) / player.maxLife;
+        //var valueFrom = player.life / player.maxLife;
+        //var valueTo = (player.life - reducationValue) / player.maxLife;
 
-        // 緑ゲージ減少
-        GreenGage.fillAmount = valueTo;
+        //// 緑ゲージ減少
+        //GreenGauge.fillAmount = valueTo;
 
-        if (redGaugeTween != null)
-        {
-            redGaugeTween.Kill();
-        }
-
-        // 赤ゲージ減少
-        redGaugeTween = DOTween.To(
-            () => valueFrom,
-            x => {
-                RedGage.fillAmount = x;
-            },
-            valueTo,
-            time
-        );
-    }
-
-    public void Update()
-    {
-        
-        //// HP上昇
-        //_hp += 0.01f;
-        //if (_hp > 1)
+        //if (redGaugeTween != null)
         //{
-        //    // 最大を超えたら0に戻す
-        //    _hp = 0;
+        //    redGaugeTween.Kill();
         //}
+
+        //// 赤ゲージ減少
+        //redGaugeTween = DOTween.To(
+        //    () => valueFrom,
+        //    x => {
+        //        RedGauge.fillAmount = x;
+        //    },
+        //    valueTo,
+        //    time
+        //);
     }
 
-    public void HPDown(float current, int max)
-    {
-        //コンポーネントのfillAmountを取得して操作する
-        GreenGage.GetComponent<Image>().fillAmount = current / max;
-    }
-    
+    //public void SetPlayer(Player player)
+    //{
+    //    this.player = player;
+    //}
 }
