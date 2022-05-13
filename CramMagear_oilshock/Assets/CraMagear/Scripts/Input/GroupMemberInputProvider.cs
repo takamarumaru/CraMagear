0beaa07402960354c09ad6eb563960df7d42baf3
MorieTakaya <kd1259599@st.kobedenshi.ac.jp>
2022-05-13 15:02:42 +0900

    【GroupMembers】 ・隊員を追加しついてくる処理を作成

--- CramMagear_oilshock/Assets/CraMagear/Scripts/Input/GroupMemberInputProvider.cs
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/Input/GroupMemberInputProvider.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GroupMemberInputProvider : InputProvider
 {
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
     //ç∂é≤éÊìæ
     public override Vector2 GetAxisL()
     {
-        return new Vector3();
+        Vector3 vectorToTheTarget  = _targetTransform.position - transform.position;
+        vectorToTheTarget.y = 0.0f;
+
+        if(vectorToTheTarget.magnitude <= 1.5f)
+        {
+            return new Vector2(0.0f, 0.0f);
+        }
+
+        vectorToTheTarget.Normalize();
+
+        return new Vector2(vectorToTheTarget.x, vectorToTheTarget.z);
     }
 
     //âEé≤éÊìæ
