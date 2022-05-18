f0d33cfb4ddd28a798f3a290a0b68b51d6fc01ce
MorieTakaya <kd1259599@st.kobedenshi.ac.jp>
2022-05-18 13:43:55 +0900

    【GameScene】 ・キャラクターにパラメーターを追加 ・右クリックで建築できるように ・仮の敵キャラクターを追加

---
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/Character/MainObjectParameter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainObjectParameter : MonoBehaviour
+{
+    [Tooltip("ƒ`[ƒ€ID")]
+    [SerializeField] int _teamID = 0;
+    public int TeamID { get=>_teamID; set => _teamID = value; }
+
+    [Tooltip("–¼‘O")]
+    [SerializeField] string _name;
+    public string Name => _name;
+
+    [Tooltip("‘Ì—Í")]
+    [SerializeField] int _hp = 0;
+    public int hp
+    { 
+        get => _hp;
+        set 
+        {
+            _hp = value; 
+            if (_hp < 0)_hp = 0;
+        } 
+    }
+
+}
