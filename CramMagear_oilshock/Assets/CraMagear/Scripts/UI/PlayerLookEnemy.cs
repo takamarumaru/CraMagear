bab74bc6b52ed540ca5ebd51ea53340262224498
yoshizawasouma <kd1254382@st.kobedenshi.ac.jp>
2022-05-30 11:55:18 +0900

    敵がいる方向に矢印が向く

--- CramMagear_oilshock/Assets/CraMagear/Scripts/UI/PlayerLookEnemy.cs
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/UI/PlayerLookEnemy.cs
@@ -6,40 +6,41 @@ using DG.Tweening;
 
 public class PlayerLookEnemy : MonoBehaviour
 {
-    [SerializeField] Transform Player;
-
-    private GameObject playerObject;
-    private Vector3 PlayerPosition; //プレイヤーの位置情報
-    private Vector3 EnemyPosition;  //敵の位置情報
+    [SerializeField] GameObject target;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject arrow;
+    //[SerializeField] GameObject Rotate;  //プレイヤーの回転
+    Vector3 RotatePos;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerObject = GameObject.FindWithTag("Player");
+        //var Scale = arrow.transform.DOScale(0.15f, 1f).SetLoops(-1, LoopType.Incremental);
 
-        PlayerPosition = playerObject.transform.position;
-        EnemyPosition = transform.position;
+        //var sequence1 = DOTween.Sequence(); //Sequence生成
+        //Rotate = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
+        arrow.transform.position += player.transform.position - RotatePos;
+        RotatePos = player.transform.position;
 
-        //プレイヤーに向かってレイを飛ばす
-        if(Physics.Raycast(transform.position, 
-                           PlayerPosition - EnemyPosition,
-                           out hit,
-                           Mathf.Infinity))
-        {
-            if(hit.collider.tag == "Player")
-            {
+        transform.RotateAround(RotatePos, Vector3.up, 50.0f * Time.deltaTime);
 
-            }
-            //PlayerPosition = playerObject.transform.position;
-            //EnemyPosition = transform.position;
+        transform.position = player.transform.position + player.transform.forward * 1.2f + Vector3.up * 0.1f;
 
-            //transform.position = EnemyPosition;
-        }
+        Vector2 vec2 = new Vector2(target.transform.position.x - player.transform.position.x,
+                           target.transform.position.z - player.transform.position.z);
+
+        float r = Mathf.Atan2(vec2.y, vec2.x);
+        float angle = Mathf.Floor(r * 360 / (2 * Mathf.PI));
+        arrow.transform.rotation = Quaternion.Euler(0, 90 - angle, 0);
+
+        //transform.DOScale(0.11f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+        //arrow.transform.DOScale(0.15f, 0.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuart);
+        
     }
+
 }
