c71a4ec46e94dff8932dbf1cbcfea7c7da4aed44
MorieTakaya <kd1259599@st.kobedenshi.ac.jp>
2022-05-30 11:55:59 +0900

    【Architecture】 ・隊員が来てから建築するように ・隊員が指定数いなかったら建築しないように

---
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/Character/ArchitectureWhenMembersCome.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchitectureWhenMembersCome : MonoBehaviour
+{
+    private List<MembersAdministrator.MemberInfo> _memberInfo = new ();
+    [SerializeField] Transform _architecturePrefab;
+
+    public void SetMember(MembersAdministrator.MemberInfo info)
+    {
+        _memberInfo.Add(info);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_memberInfo.Count == 0) return;
+
+        bool isArrival = true;
+        foreach (MembersAdministrator.MemberInfo info in _memberInfo)
+        {
+            isArrival &= info._isArrival;
+        }
+        if(isArrival)
+        {
+            Transform architectureTransform = Instantiate(_architecturePrefab,transform.position, transform.rotation);
+            for (int memberIdx = 0; memberIdx < _memberInfo.Count; memberIdx++)
+            {
+                _memberInfo[memberIdx]._input.TargetTransform = architectureTransform;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
