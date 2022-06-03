be2a1d583e0c8dc665d026ff82c8829d81957c29
yoshizawasouma <kd1254382@st.kobedenshi.ac.jp>
2022-06-03 14:55:44 +0900

    フェーズ1のタイマー実装

---
+++ CramMagear_oilshock/Assets/CraMagear/Scripts/UI/PhaseTimer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhaseTimer : MonoBehaviour
+{
+    //　制限時間（分）
+    //[SerializeField]
+    //private int minute;
+    //　制限時間（秒）
+    //[SerializeField]
+    //private float seconds;
+    [SerializeField]
+    GameAadministrator CountLimitTime;
+
+    public static float CountDownTime;    // カウントダウンタイム
+    public Text TextCountDown;              // 表示用テキストUI
+    [SerializeField]
+    public Image countDownImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CountDownTime = CountLimitTime.FirstPhaseTime;
+        //CountDownTime = GameAadministrator.Instance.FirstPhaseCount;
+        //CountDownTime = 60.0F;    // カウントダウン開始秒数をセット
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // カウントダウンタイムを整形して表示
+        TextCountDown.text = string.Format("Phase1: {0:00:00}", CountDownTime);
+        countDownImage.fillAmount = CountDownTime % 1.0f; ;
+
+        // 経過時刻を引いていく
+        CountDownTime -= Time.deltaTime;
+        
+        if(CountDownTime <= 0)
+        {
+            CountDownTime = 0;
+        }
+    }
+}
